e54182cc67c696a40407c362fdc25f1703c01c58
Nicolas Zin <nicolas.zin@savoirfairelinux.com>
2017-06-26 13:22:05 -0400

    improvement

--- dock.go
+++ dock.go
@@ -10,6 +10,9 @@ type DockWidget struct {
 	sws.SWS_CoreWidget
 	currentDay    *sws.SWS_Label
 	timer         *Timer
+	pause         *sws.SWS_FlatButtonWidget
+	play          *sws.SWS_FlatButtonWidget
+	forward       *sws.SWS_FlatButtonWidget
 	shop          *sws.SWS_FlatButtonWidget
 	quit          *sws.SWS_FlatButtonWidget
 	timerevent    *sws.TimerEvent
@@ -24,35 +27,42 @@ func (self *DockWidget) SetShopCallback(callback func()) {
 }
 
 func CreateDockWidget(timer *Timer) *DockWidget {
-	corewidget := sws.CreateCoreWidget(100, 100)
+	corewidget := sws.CreateCoreWidget(150, 100)
 	today:=fmt.Sprintf("%d %s %d",timer.CurrentTime.Day(),timer.CurrentTime.Month().String(),timer.CurrentTime.Year())
 	widget := &DockWidget { 
 		SWS_CoreWidget: *corewidget,
-		currentDay: sws.CreateLabel(100,25,today),
+		currentDay: sws.CreateLabel(150,25,today),
 		timer: timer,
 		timerevent: nil,
 	}
-	title:=sws.CreateLabel(100,25,"DC Tycoon")
+	title:=sws.CreateLabel(150,25,"DC Tycoon")
 	title.SetCentered(true)
 	widget.AddChild(title)
 	
-	widget.currentDay.Move(0,25)
+	widget.currentDay.Move(5,25)
 	widget.AddChild(widget.currentDay)
 	
-	pause:=sws.CreateFlatButtonWidget(25,25,"")
-	pause.Move(0,50)
-	pause.SetImage("resources/icon-pause-symbol.png")
-	pause.SetClicked(func() {
+	widget.pause=sws.CreateFlatButtonWidget(25,25,"")
+	widget.pause.SetColor(0xff8888ff)
+	widget.pause.Move(25,50)
+	widget.pause.SetImage("resources/icon-pause-symbol.png")
+	widget.pause.SetClicked(func() {
+		widget.pause.SetColor(0xff8888ff)
+		widget.play.SetColor(0xffdddddd)
+		widget.forward.SetColor(0xffdddddd)
 		if widget.timerevent!=nil {
 			widget.timerevent.StopRepeat()
 		}
 	})
-	widget.AddChild(pause)
+	widget.AddChild(widget.pause)
 	
-	play:=sws.CreateFlatButtonWidget(25,25,"")
-	play.Move(25,50)
-	play.SetImage("resources/icon-arrowhead-pointing-to-the-right.png")
-	play.SetClicked(func() {
+	widget.play=sws.CreateFlatButtonWidget(25,25,"")
+	widget.play.Move(50,50)
+	widget.play.SetImage("resources/icon-arrowhead-pointing-to-the-right.png")
+	widget.play.SetClicked(func() {
+		widget.pause.SetColor(0xffdddddd)
+		widget.play.SetColor(0xff8888ff)
+		widget.forward.SetColor(0xffdddddd)
 		if widget.timerevent!=nil {
 			widget.timerevent.StopRepeat()
 		}
@@ -62,12 +72,15 @@ func CreateDockWidget(timer *Timer) *DockWidget {
 			widget.currentDay.SetText(today)
 		})
 	})
-	widget.AddChild(play)
+	widget.AddChild(widget.play)
 	
-	forward:=sws.CreateFlatButtonWidget(25,25,"")
-	forward.Move(50,50)
-	forward.SetImage("resources/icon-forward-button.png")
-	forward.SetClicked(func() {
+	widget.forward=sws.CreateFlatButtonWidget(25,25,"")
+	widget.forward.Move(75,50)
+	widget.forward.SetImage("resources/icon-forward-button.png")
+	widget.forward.SetClicked(func() {
+		widget.pause.SetColor(0xffdddddd)
+		widget.play.SetColor(0xffdddddd)
+		widget.forward.SetColor(0xff8888ff)
 		if widget.timerevent!=nil {
 			widget.timerevent.StopRepeat()
 		}
@@ -77,25 +90,25 @@ func CreateDockWidget(timer *Timer) *DockWidget {
 			widget.currentDay.SetText(today)
 		})
 	})
-	widget.AddChild(forward)
+	widget.AddChild(widget.forward)
 	
 	widget.shop=sws.CreateFlatButtonWidget(25,25,"")
-	widget.shop.Move(0,75)
+	widget.shop.Move(25,75)
 	widget.shop.SetImage("resources/icon-shopping-cart-black-shape.png")
 	widget.AddChild(widget.shop)
 	
 	inventory:=sws.CreateFlatButtonWidget(25,25,"")
-	inventory.Move(25,75)
+	inventory.Move(50,75)
 	inventory.SetImage("resources/icon-delivery-truck-silhouette.png")
 	widget.AddChild(inventory)
 	
 	save:=sws.CreateFlatButtonWidget(25,25,"")
-	save.Move(50,75)
+	save.Move(75,75)
 	save.SetImage("resources/icon-blank-file.png")
 	widget.AddChild(save)
 	
 	widget.quit=sws.CreateFlatButtonWidget(25,25,"")
-	widget.quit.Move(75,75)
+	widget.quit.Move(100,75)
 	widget.quit.SetImage("resources/icon-power-button-off.png")
 	widget.AddChild(widget.quit)
 	
