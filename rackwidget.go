7b5baddb5e60939b3055d5d4049ba004de91c36a
Nicolas Zin <nicolas.zin@savoirfairelinux.com>
2017-07-19 01:22:46 -0400

    work on ui

--- rackwidget.go
+++ rackwidget.go
@@ -3,7 +3,12 @@ package dctycoon
 import (
 	"fmt"
 	"github.com/nzin/sws"
+	"github.com/veandco/go-sdl2/sdl"
 	"github.com/nzin/dctycoon/supplier"
+)
+
+const (
+	CHASSIS_OFFSET=35
 )
 
 type ServerDragPayload struct {
@@ -45,6 +50,44 @@ func NewRackWidgetLine(item *supplier.InventoryItem) *RackWidgetLine{
 	}
 }
 
+type RackWidgetItems struct {
+	sws.SWS_CoreWidget
+	vbox   *sws.SWS_VBoxWidget
+	scroll *sws.SWS_ScrollWidget
+}
+
+func NewRackWidgetItems(inventory *supplier.Inventory) *RackWidgetItems {
+	widgetitems:=&RackWidgetItems{
+		SWS_CoreWidget: *sws.CreateCoreWidget(300,100),
+		vbox: sws.CreateVBoxWidget(300,0),
+		scroll: sws.CreateScrollWidget(300,300),
+	}
+	inventory.AddSubscriber(widgetitems)
+	
+	label:=sws.CreateLabel(300,25,"Available server to place: ")
+	widgetitems.AddChild(label)
+	
+	widgetitems.scroll.ShowHorizontalScrollbar(false)
+	widgetitems.scroll.SetInnerWidget(widgetitems.vbox)
+	widgetitems.scroll.Move(0,25)
+	widgetitems.AddChild(widgetitems.scroll)
+	
+	return widgetitems
+}
+
+func (self *RackWidgetItems) Resize(w,h int32) {
+	self.scroll.Resize(w,h-25)
+}
+
+func (self *RackWidgetItems) ItemInTransit(item *supplier.InventoryItem) {
+}
+
+func (self *RackWidgetItems) ItemInStock(item *supplier.InventoryItem) {
+	if item.Typeitem==supplier.PRODUCT_SERVER && item.Serverconf.ConfType.NbU>0 && item.Xplaced==-1 {
+		self.vbox.AddChild(NewRackWidgetLine(item))
+	}
+}
+
 type RackChassisWidget struct {
 	sws.SWS_CoreWidget
 	inventory  *supplier.Inventory
@@ -54,7 +97,7 @@ type RackChassisWidget struct {
 }
 
 func (self *RackChassisWidget) computeComingPos(ypos int32) int32 {
-	ypos=(ypos-10)/10
+	ypos=(ypos-CHASSIS_OFFSET)/10
 	nbu:=self.comingitem.Serverconf.ConfType.NbU
 	var busy [42]bool
 	
@@ -95,30 +138,37 @@ func (self *RackChassisWidget) computeComingPos(ypos int32) int32 {
 func (self *RackChassisWidget) Repaint() {
 	self.SWS_CoreWidget.Repaint()
 	
-	self.SetDrawColor(0, 0, 0, 255)
-	self.DrawLine(9,9,110,9)
-	self.DrawLine(110,9,110,430)
-	self.DrawLine(110,430,9,430)
-	self.DrawLine(9,430,9,9)
+	var watts float64
+	for _,i := range(self.items) {
+		watts+=i.Serverconf.PowerConsumption()
+	}
+	rackwatt:=fmt.Sprintf("Rack consumption: %.2f W",watts)
+	self.WriteText(10,10,rackwatt,sdl.Color{0,0,0,255})
 	
-	self.FillRect(10, 10, 100,420, 0xffaaaaaa)
+	self.SetDrawColor(0, 0, 0, 255)
+	self.DrawLine(9,CHASSIS_OFFSET-1,110,CHASSIS_OFFSET-1)
+	self.DrawLine(110,CHASSIS_OFFSET-1,110,420+CHASSIS_OFFSET)
+	self.DrawLine(110,420+CHASSIS_OFFSET,9,420+CHASSIS_OFFSET)
+	self.DrawLine(9,420+CHASSIS_OFFSET,9,CHASSIS_OFFSET-1)
+	
+	self.FillRect(10, CHASSIS_OFFSET, 100,420, 0xffaaaaaa)
 	
 	if (self.ydrag!=-1) {
 		ypos:=self.computeComingPos(self.ydrag)
 		if ypos!=-1 {
 			nbu:=self.comingitem.Serverconf.ConfType.NbU
 			self.SetDrawColor(255, 0, 0, 255)
-			self.DrawLine(10,10+ypos*10,19,10+ypos*10)
-			self.DrawLine(10,10+ypos*10,10,19+ypos*10)
+			self.DrawLine(10,CHASSIS_OFFSET+ypos*10,19,CHASSIS_OFFSET+ypos*10)
+			self.DrawLine(10,CHASSIS_OFFSET+ypos*10,10,CHASSIS_OFFSET+9+ypos*10)
 
-			self.DrawLine(109,10+ypos*10,100,10+ypos*10)
-			self.DrawLine(109,10+ypos*10,109,19+ypos*10)
+			self.DrawLine(109,CHASSIS_OFFSET+ypos*10,100,CHASSIS_OFFSET+ypos*10)
+			self.DrawLine(109,CHASSIS_OFFSET+ypos*10,109,CHASSIS_OFFSET+9+ypos*10)
 
-			self.DrawLine(10,9+(ypos+nbu)*10,19,9+(ypos+nbu)*10)
-			self.DrawLine(10,9+(ypos+nbu)*10,10,0+(ypos+nbu)*10)
+			self.DrawLine(10,CHASSIS_OFFSET-1+(ypos+nbu)*10,19,CHASSIS_OFFSET-1+(ypos+nbu)*10)
+			self.DrawLine(10,CHASSIS_OFFSET-1+(ypos+nbu)*10,10,CHASSIS_OFFSET-10+(ypos+nbu)*10)
 
-			self.DrawLine(109,9+(ypos+nbu)*10,100,9+(ypos+nbu)*10)
-			self.DrawLine(109,9+(ypos+nbu)*10,109,0+(ypos+nbu)*10)
+			self.DrawLine(109,CHASSIS_OFFSET-1+(ypos+nbu)*10,100,CHASSIS_OFFSET-1+(ypos+nbu)*10)
+			self.DrawLine(109,CHASSIS_OFFSET-1+(ypos+nbu)*10,109,CHASSIS_OFFSET-10+(ypos+nbu)*10)
 		}
 	}
 }
@@ -154,7 +204,7 @@ func (self *RackChassisWidget) DragDrop(x,y int32, payload sws.DragPayload) {
 
 func NewRackChassisWidget(inventory *supplier.Inventory) *RackChassisWidget {
 	chassis:=&RackChassisWidget {
-		SWS_CoreWidget: *sws.CreateCoreWidget(300,600),
+		SWS_CoreWidget: *sws.CreateCoreWidget(220,430+CHASSIS_OFFSET),
 		inventory: inventory,
 		ydrag: -1,
 		items: make([]*supplier.InventoryItem,0),
@@ -176,52 +226,56 @@ type RackWidget struct {
 	xactiveElement int32
 	yactiveElement int32
 	activeElement  DcElement
-	vbox           *sws.SWS_VBoxWidget
 	splitview      *sws.SWS_SplitviewWidget
 }
 
 func NewRackWidget(rootwindow *sws.SWS_RootWidget,inventory *supplier.Inventory) *RackWidget {
 	mainwidget:=sws.CreateMainWidget(650,400," Rack info ",false,true)
-	sv:=sws.CreateSplitviewWidget(400,300,true)
+	svBottom:=sws.CreateSplitviewWidget(400,300,true)
 	
 	rack:=&RackWidget{
-		SWS_CoreWidget: *sws.CreateCoreWidget(650,400),
 		mainwidget: mainwidget,
 		rootwindow: rootwindow,
 		inventory: inventory,
 		xactiveElement: -1,
 		yactiveElement: -1,
 		activeElement: nil,
-		vbox: sws.CreateVBoxWidget(300,0),
-		splitview: sv,
+		splitview: svBottom,
 	}
-	inventory.AddSubscriber(rack)
-	
-	scrollleft:=sws.CreateScrollWidget(300,300)
-	scrollleft.ShowHorizontalScrollbar(false)
-	scrollleft.SetInnerWidget(rack.vbox)
-	scrollleft.SetColor(0xffffffff)
-	
-	sv.PlaceSplitBar(300)
+
+	sv := sws.CreateSplitviewWidget(200,200,false)
+	sv.PlaceSplitBar(50)
 	sv.SplitBarMovable(false)
-	sv.SetLeftWidget(scrollleft)
+	mainwidget.SetInnerWidget(sv)
+
+	menu:=sws.CreateCoreWidget(500,50)
+	menu.SetColor(0xffffffff)
+	sv.SetLeftWidget(menu)
+	
+	menuservers:=sws.CreateLabel(300,50,"Servers")
+	menuservers.SetColor(0xffffffff)
+	menuservers.SetCentered(true)
+	menu.AddChild(menuservers)
+	
+	menurack:=sws.CreateLabel(300,50,"Rack chassis")
+	menurack.SetColor(0xffffffff)
+	menurack.Move(300,0)
+	menu.AddChild(menurack)
+	
+	svBottom.PlaceSplitBar(300)
+	svBottom.SplitBarMovable(false)
+	svBottom.SetLeftWidget(NewRackWidgetItems(inventory))
 
 	scrollright:=sws.CreateScrollWidget(200,300)
 	scrollright.SetInnerWidget(NewRackChassisWidget(inventory))
-	sv.SetRightWidget(scrollright)
+	svBottom.SetRightWidget(scrollright)
 	
-	rack.AddChild(sv)
-
-	mainwidget.SetInnerWidget(rack)
+	sv.SetRightWidget(svBottom)
+	
 	mainwidget.SetCloseCallback(func() {
 		rack.Hide()
 	})
 	return rack
-}
-
-func (self *RackWidget) Resize(w,h int32) {
-	self.SWS_CoreWidget.Resize(w,h)
-	self.splitview.Resize(w,h)
 }
 
 func (self *RackWidget) Show() {
@@ -236,15 +290,3 @@ func (self *RackWidget) Hide() {
 		self.rootwindow.SetFocus(children[0])
 	}
 }
-
-func (self *RackWidget) ItemInTransit(item *supplier.InventoryItem) {
-}
-
-func (self *RackWidget) ItemInStock(item *supplier.InventoryItem) {
-	fmt.Println("RackWidget::ItemInStock")
-	if item.Typeitem==supplier.PRODUCT_SERVER && item.Serverconf.ConfType.NbU>0 && item.Xplaced==-1 {
-		fmt.Println("RackWidget::ItemInStock b")
-		self.vbox.AddChild(NewRackWidgetLine(item))
-	}
-}
-
