a424840d0902eb6ef95c205ab4daa6ba0d284051
Nicolas Zin <nicolas.zin@savoirfairelinux.com>
2017-12-26 03:37:18 -0500

    cron doesn't take into account skipped days

--- timer/gametimer_test.go
+++ timer/gametimer_test.go
@@ -51,4 +51,9 @@ func TestCronGameTimer(t *testing.T) {
 	}
 	gt.TimerClock()
 	assert.Equal(t, 3, month, "1/3, beginning of march")
+
+	// skip some days...
+	gt.CurrentTime = time.Date(1990, 4, 3, 1, 1, 1, 0, time.UTC)
+	gt.TimerClock()
+	assert.NotEqual(t, 4, month, "pass april")
 }
