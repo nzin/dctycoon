cff95975386704d9b0ebf35c158cb790a892aa49
Nicolas Zin <nicolas.zin@gmail.com>
2018-01-19 01:02:05 -0500

    instructions how to add icons

--- compile.sh
+++ compile.sh
@@ -2,4 +2,8 @@
 
 go get -u github.com/jteeuwen/go-bindata/...
 $(go env GOPATH)/bin/go-bindata -o global/assets.go -pkg global -ignore *.sh assets/...
+# on windows:
+# go build -ldflags "-H windowsgui" -o dctycoon ./main
+
+# for macosx, see https://github.com/Xeoncross/macappshell
 go build -o dctycoon ./main
